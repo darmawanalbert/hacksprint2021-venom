19d189139f8b718d11534f83bfb54d680e75cd53
Albert Darmawan <darmawanalbert@gmail.com>
2021-07-12 10:28:37 +0700

    Simple animation for Emoji

--- waistd/components/Emoji.js
+++ waistd/components/Emoji.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { Easing, Animated } from 'react-native';
 import { Planet } from 'react-kawaii/lib/native';
 import colors from '../utils/colors';
 
@@ -16,8 +17,43 @@ function Emoji({ size = 100, mood }) {
     else {
         moodString = "happy";
     }
+
+    const yTranslation = useRef(new Animated.Value(0)).current;
+    const runAnimation = () => {
+        return Animated.loop(
+            Animated.sequence([
+                Animated.timing(yTranslation, {
+                    toValue: 20,
+                    duration: 1000,
+                    easing: Easing.linear,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(yTranslation, {
+                    toValue: 0,
+                    duration: 1000,
+                    easing: Easing.linear,
+                    useNativeDriver: true,
+                })
+            ]),
+            {resetBeforeIteration: true, iterations: Number.MAX_SAFE_INTEGER},
+        ).start();
+    }
+
+    useEffect(() => {
+        const animation = runAnimation();
+        // return () => animation.stop();
+    }, []);
+
     return (
-        <Planet size={size} mood={moodString} color={colors[mood]} />
+        <Animated.View
+            style={{
+                transform: [
+                    { translateY: yTranslation}
+                ]
+            }}
+        >
+            <Planet size={size} mood={moodString} color={colors[mood]} />
+        </Animated.View>
     );
 }
 
